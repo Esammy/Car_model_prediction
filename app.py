19305230920e055ea5ecd001eac98747c973c8ef
Esammy <egwusamuel2015@gmail.com>
2023-08-03 14:56:17 +0100

    pred_func

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify
 from ultralytics import YOLO
+# import sympy
 import numpy as np
 
-model = YOLO('./weights/best.pt')
+model = YOLO('./weights/last.pt')
 
 
 app = Flask(__name__)
@@ -10,6 +11,13 @@ app = Flask(__name__)
 # Optionally, you can set the app to run in debug mode during development
 app.config['DEBUG'] = True
 
+def predict_cars(input_data):
+    result = model(input_data)
+    names_dict = result[0].names
+    prob_score = result[0].probs.tolist()
+    pred_car = names_dict[np.argmax(prob_score)]
+
+    return {"car_make_model": pred_car}
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -18,15 +26,11 @@ def predict():
         data = request.json
 
         # Call your prediction model to get the result
-        result = model(data)
-        names_dict = result[0].names
-        prob = result[0].probs.tolist()
-        pred_car = names_dict[np.argmax(prob)]
-
-
+        prediction_result = predict_cars(data)
 
         # Return the prediction result as a JSON response
-        return jsonify({'prediction': pred_car})
+        return jsonify(prediction_result)
+    
     except Exception as e:
         return jsonify({'error': str(e)})
 
